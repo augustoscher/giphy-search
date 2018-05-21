f8dad7fc56192ba0efd78f35a8f5e76037780dde
AUGUSTO-PC\Augusto-Workstation <augustoscher@gmail.com>
2018-05-21 01:51:44 -0300

    useHash on lazy routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-@NgModule({
-    imports: [RouterModule.forRoot([
+const routes: Routes = [
         //Colocar rotas lazy loading
         //passa o path e o caminho absoluto do modulo da pagina, seguido de # pelo nome da classe do modulo
         { path: 'giphy-search-auto', loadChildren: 'app/giphy/giphy-search-auto/giphy-search-auto.module#GiphySearchAutoModule' },
         { path: 'giphy', loadChildren: 'app/giphy/giphy-search-manual/giphy-search-manual.module#GiphySearchComponentModule' },
         { path: 'diretiva-common-module', loadChildren: 'app/diretiva-commom-module/diretiva-commom-module.module#DiretivaCommomModuleModule' }
-    ])],
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes, { useHash: true })],
     exports: [RouterModule],
     declarations: [],
     providers: []
